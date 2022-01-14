2d40fd9ffb3c9335aa5788bea6355c702f79842c
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-01-14 18:19:03 +0700

    build: update buildfiles

--- .versionrc.js
+++ .versionrc.js
@@ -3,7 +3,7 @@ const defaultStandardVersion = require("@dnb-org/standard-version-config");
 const localStandardVersion = {
   bumpFiles: [
     ...defaultStandardVersion.bumpFiles,
-    { filename: "data;dnb/security/version.json", type: "json" },
+    { filename: "data/dnb/security/version.json", type: "json" },
   ],
 };
 
